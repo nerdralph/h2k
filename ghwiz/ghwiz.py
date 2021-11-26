4f286fc9dc658e678746c23a561e2b678bd6e548
U-parsons-PC\parsons <ralphdoncaster@gmail.com>
2021-11-26 17:57:18 -0400

    21-11-26 snapshot

---
+++ ghwiz/ghwiz.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import sys
+import xml.etree.ElementTree as ET
+
+FT_PER_M = 3.28084
+SF_PER_SM = FT_PER_M ** 2
+
+#def set_val(tree, path, value):
+#    tree.find(path).attrib["value"] = value
+
+if len(sys.argv) < 3:
+    print(sys.argv[0], "FacingDirection YearBuilt operim oarea")
+    sys.exit()
+
+FacingDirection=sys.argv[1]
+YearBuilt=sys.argv[2]
+operim=float(sys.argv[3])
+oarea=float(sys.argv[4])
+
+fileid="22RDD00067"
+
+#t = ET.parse("template.h2k")
+t = ET.parse("tmpl-oil.h2k")
+
+t.find("./ProgramInformation/File").attrib["evaluationDate"] = "2021-11-17"
+#t.find("./ProgramInformation/File/Identification").text = "22RDD00067"
+t.find("./ProgramInformation/File/Identification").text = fileid
+t.find("./House/Specifications/FacingDirection").attrib["code"] = FacingDirection
+t.find("./House/Specifications/YearBuilt").attrib["value"] = YearBuilt
+
+# calculate foundation and main floor area converted to metric
+main_area=(oarea-operim/2+1)/SF_PER_SM
+bsmt_area=(oarea-operim+4)/SF_PER_SM
+hfa = t.find("./House/Specifications/HeatedFloorArea")
+hfa.attrib["aboveGrade"] = str(main_area)
+hfa.attrib["belowGrade"] = str(bsmt_area)
+
+# calculate volume 7.75ft bsmt + header + 8ft main flr
+volume=(7.75/FT_PER_M * bsmt_area) + (1 + 8)/FT_PER_M * main_area;
+t.find(".House/NaturalAirInfiltration/Specifications/House").attrib["volume"] = str(volume)
+m = t.find("House/Components/Ceiling/Measurements")
+m.attrib["length"] = str(operim/FT_PER_M)
+m.attrib["area"] = str(main_area)
+
+#t.write("ghwiz-out.h2k", "UTF-8", True)
+t.write(fileid + ".h2k", "UTF-8", True)
+
