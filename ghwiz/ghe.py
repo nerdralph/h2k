22e69bec02b5aef7383379080af09b97e3e1c0ff
U-parsons-PC\parsons <ralphdoncaster@gmail.com>
2021-12-20 17:21:35 -0400

    extract photos

--- ghwiz/ghe.py
+++ ghwiz/ghe.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from datetime import date
 import math, os, sys
 import xml.etree.ElementTree as ET
 
@@ -7,12 +8,12 @@ if len(sys.argv) < 3:
     print(sys.argv[0], "D.h2k E-ID")
     sys.exit()
 
-ea = "22RD"
 d_file = sys.argv[1]
-fileid = ea + sys.argv[2]
+fileid = sys.argv[2]
 
 t = ET.parse(d_file)
 
+t.find("./ProgramInformation/File").attrib["evaluationDate"] = date.today().isoformat()
 t.find("./ProgramInformation/File/PreviousFileId").text = \
     t.find("./ProgramInformation/File/Identification").text
 t.find("./ProgramInformation/File/Identification").text = fileid
@@ -21,10 +22,11 @@ t.find("./ProgramInformation/File/UserTelephone").text = "902-401-7056"
 t.find("./ProgramInformation/File/CompanyTelephone").text = "902-469-1119"
 
 t.getroot().remove(t.find("./AllResults"))
-#t.getroot().remove(t.find("./Program/Results"))
 t.find("./Program").remove(t.find("./Program/Results"))
 t.getroot().remove(t.find("./EnergyUpgrades"))
 
 outfile = fileid + ".h2k"
 t.write(outfile, "UTF-8", True)
 
+# extract photos
+os.system("./photos.py " + fileid)
