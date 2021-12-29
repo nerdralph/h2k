ee1c5411cad8f95f361730c92b3cd66d7bff7960
U-parsons-PC\parsons <ralphdoncaster@gmail.com>
2021-12-29 09:39:23 -0400

    modularize

--- ghwiz/photos.py
+++ ghwiz/photos.py
@@ -4,23 +4,25 @@
 import os, sys
 import zipfile
 
-FileID = sys.argv[1]
-path = "../../" + FileID + "/"
-os.mkdir(path)
+def ea_photos(file_id: str):
+    path = "../../" + file_id + "/"
+    os.mkdir(path)
+    
+    zipname = "Photos.zip"
+    zf = zipfile.ZipFile(zipname)
+    
+    file_count = 0
+    suffix = ""
+    for name in zf.namelist():
+        f = open(path + file_id + suffix + ".jpg", 'wb')
+        f.write(zf.read(name))
+        f.close()
+        file_count += 1
+        suffix = "-" + str(file_count)
+    
+    house = path + file_id + ".jpg"
+    os.link(house, "../../" + file_id + ".jpg")
+    os.remove(zipname)
 
-zipname = "Photos.zip"
-zf = zipfile.ZipFile(zipname)
-
-file_count = 0
-suffix = ""
-for name in zf.namelist():
-    f = open(path + FileID + suffix + ".jpg", 'wb')
-    f.write(zf.read(name))
-    f.close()
-    file_count += 1
-    suffix = "-" + str(file_count)
-
-house = path + FileID + ".jpg"
-os.link(house, "../../" + FileID + ".jpg")
-os.remove(zipname)
-
+if __name__ == "__main__":
+    ea_photos(sys.argv[1])
