71f175d3acbaeda4eaf8eb71c13830107a33c94c
U-parsons-PC\parsons <ralphdoncaster@gmail.com>
2022-01-08 20:56:00 -0400

    determine photo date from IMG filename

--- ghwiz/photos.py
+++ ghwiz/photos.py
@@ -5,21 +5,24 @@ import os, sys
 import zipfile
 
 # iso8601 is YYYY-MM-DD
-def iso8601(dt) -> str:
-    ymd = [str(d) for d in dt[0:3]]
-    return "-".join(ymd)
+#def iso8601(dt) -> str:
+#    ymd = [str(d) for d in dt[0:3]]
+#    return "-".join(ymd)
 
 # extract photos & return ISO8601 date of first photo
 def extract(file_id: str) -> str:
     path = "../../" + file_id + "/"
     os.mkdir(path)
     
-    zipname = "Photos.zip"
+    zipname = "Photos-001.zip"
     zf = zipfile.ZipFile(zipname)
     
     file_count = 0
     suffix = ""
-    for name in zf.namelist():
+    # filename = IMG_YYYYMMDD_HHMMSSmmm, so sort to put earliest first
+    nl = zf.namelist()
+    nl.sort()
+    for name in nl:
         f = open(path + file_id + suffix + ".jpg", 'wb')
         f.write(zf.read(name))
         f.close()
@@ -29,8 +32,13 @@ def extract(file_id: str) -> str:
     house = path + file_id + ".jpg"
     os.link(house, "../../" + file_id + ".jpg")
     os.remove(zipname)
-    return iso8601(zf.infolist()[0].date_time)
+
+    #return iso8601(zf.infolist()[0].date_time)
+
+    h = nl[0]
+    iso8601 = h[4:8] + '-' + h[8:10] + '-' + h[10:12]
+    return iso8601
 
 
 if __name__ == "__main__":
-    extract(sys.argv[1])
+    print (extract(sys.argv[1]))
