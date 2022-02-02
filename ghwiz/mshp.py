e9cef85888f6c46e2d2c2c5562979bf87105845c
U-parsons-PC\parsons <ralphdoncaster@gmail.com>
2022-02-02 10:57:39 -0400

    key on AHRI #

--- ghwiz/mshp.py
+++ ghwiz/mshp.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python
 # configure mini-split heat pumps for E files
 
-import math, os, sys
+import csv, math, os, sys
 import xml.etree.ElementTree as ET
 
-if len(sys.argv) < 4:
-    print(sys.argv[0], "E-file.h2k mfr model heads")
+if len(sys.argv) < 3:
+    print(sys.argv[0], "E-file.h2k AHRI heads")
     sys.exit()
 
 e_file = sys.argv[1]
-mfr = sys.argv[2].upper()
-model = sys.argv[3].upper()
-heads = sys.argv[4]
+ahri = sys.argv[2]
+heads = sys.argv[3]
 
 t = ET.parse(e_file)
 
-cchp_search = "grep -i '" + mfr + ".*" + model + "' ccashp.tsv|grep -v Ducted"
+# tsv field list: 
+# Product group	AHRI  No.	Brand Name	Series Name/Product Line (if applicable)	Outdoor Unit Model	System type	Indoor Type/ Ducting Configuration	Indoor Unit - Model Number  (Evaporator and/or Air Handler)	Furnace Model Number (if applicable)	Rated heating capacity 8.3°C/47°F  Btu/hour 	HSPF  (Region IV)  Btu/(W⋅h)	COP at Max. Capacity  -15°C/5°F	Capacity Maintenance %  (Max -15°C/5°F ÷ Rated 8.3°C/47°F) Btu/hour 	Rated Cooling Capacity 35°C/95°F   Btu/hour	SEER  Btu/(W⋅h)	Grant amount
+cchp_search = "grep -i '" + ahri + "' ccashp.tsv|grep -v Ducted"
 d = os.popen(cchp_search).read().split('\t')
-(ahri, size_kw, hspf, cop, seer) = d[1], str(float(d[9])/3412), d[10], d[11], d[14]
+(mfr, model, size_kw, hspf, cop, seer) = d[2], d[4], str(float(d[9])/3412), d[10], d[11], d[14]
+#cchp_search = "grep -i '" + mfr + ".*" + model + "' ccashp.csv"
+#row = os.popen(cchp_search).read()
+#cols = next([row])
+#(ahri, size_kw, hspf, cop, seer) = cols[9], str(float(cols[5])/3412), cols[4], cols[13], cols[12] 
 
 e = t.find("./ProgramInformation/Information")
 
@@ -54,6 +59,7 @@ hc = t.find("./House/HeatingCooling")
 hc.remove(hc.find("Type2"))
 hc.append(type2)
 
+#outfile = "MSHP-out.h2k"
 outfile = e_file
 t.write(outfile, "UTF-8", True)
 
