9127d3e85e22404efcd1a9d729a9bf0ac7bc60e1
U-parsons-PC\parsons <ralphdoncaster@gmail.com>
2022-04-15 11:18:07 -0300

    add afl_perim optional arg

--- ghwiz/ghwiz.py
+++ ghwiz/ghwiz.py
@@ -11,7 +11,8 @@ FT_PER_M = 3.28084
 SF_PER_SM = FT_PER_M ** 2
 
 if len(sys.argv) < 6:
-    print(sys.argv[0], "fileid template.h2k wall-height operim barea [tadelta]")
+    print(sys.argv[0], "fileid template.h2k afl-height fperim farea [ta_delta] [afl_perim]")
+    print("f = foundation, t = top floor, outside measurements")
     sys.exit()
 
 args = sys.argv
@@ -19,12 +20,18 @@ fileid = args.pop(1)
 template = args.pop(1)
 # wall height in metres
 wall_height_m = float(args.pop(1))/FT_PER_M
-# outside wall perimeter
+# outside foundation perimeter
 operim = float(args.pop(1))
-# basement exterior area
+# foundation exterior area
 barea = float(args.pop(1))
+if barea < 0:
+    print("Invalid foundation area ", barea)
+    sys.exit()
+
 # top floor exterior area difference from barea
 ta_delta = float(args.pop(1)) if len(args) > 1 else 0
+# above foundation perimeter if different than fperim
+afl_perim = float(args.pop(1)) if len(args) == 1 else operim
 
 t = ET.parse(template)
 
@@ -39,6 +46,12 @@ info = input("client info: ")
 if rest[0:3] == " NS":
     rest = rest[3:]
 (postal, name, tel) = rest[1:8], rest[9:-10],rest[-12:]
+
+# copy stick-framed 2x6 house specs
+house_data = "../../" + fileid  + "/" + fileid + "-house-data.txt"
+os.system("cp 2x6-house.txt " + house_data)
+hd = open(house_data, 'a')
+hd.write(info)
 
 c = t.find("./ProgramInformation/Client")
 c.find("Name/First").text = name.split(' ')[0]
@@ -58,62 +71,86 @@ t.find("ProgramInformation/File/Identification").text = fileid
 storeys = 2 if wall_height_m > 4 else 1
 # code 1 = 1 storey, 3 = 2 storey
 t.find("House/Specifications/Storeys").attrib["code"] = "1" if storeys == 1 else "3"
+hd.write("\nstoreys: " + str(storeys))
 
 # calculate foundation and main floor area converted to metric
-main_area_sm=(barea-operim/2+1)/SF_PER_SM
-mperim_m = (operim-4)/FT_PER_M
-bsmt_area_sm=(barea-operim+4)/SF_PER_SM
-bperim_m = (operim-8)/FT_PER_M
-# assume top perim change of 3 * sqrt ta_delta, corrected for inside area
-# consider sign since ta_delta can be negative
+main_area_sm=(barea - afl_perim/2 +1)/SF_PER_SM
+mperim_in_m = (afl_perim -4)/FT_PER_M
+bsmt_area_sm=(barea -operim +4)/SF_PER_SM
+bperim_in_m = (operim -8)/FT_PER_M
+
+# calculate sign since ta_delta can be negative
 if ta_delta != 0:
     ta_sign = math.sqrt(pow(ta_delta,2))/ta_delta
 else:
     ta_sign = 1
 
+# ta_delta is exterior area so reduce by sqrt for rough interior area
 ta_sqrt = math.sqrt(abs(ta_delta))
-tad_in_sm = (abs(ta_delta) - ta_sqrt)/SF_PER_SM
-tp_delta_m = math.sqrt(abs(tad_in_sm)) * 3 * ta_sign
+tad_in_sm = (ta_delta - ta_sqrt * ta_sign)/SF_PER_SM
 hfa = t.find("House/Specifications/HeatedFloorArea")
-hfa.attrib["aboveGrade"] = str(main_area_sm * storeys + tad_in_sm * ta_sign)
+above_grade_sm = (main_area_sm * storeys) + tad_in_sm
+hfa.attrib["aboveGrade"] = str(above_grade_sm)
 hfa.attrib["belowGrade"] = str(bsmt_area_sm)
+hd.write("\nheated floor area sf above grade: " + str(round(above_grade_sm * SF_PER_SM))
+            + " below grade: " + str(round(bsmt_area_sm * SF_PER_SM)))
 
 # calculate volume 7.75ft bsmt + 1' header + 8ft main flr
 volume = ((7.75 + 1)/FT_PER_M * bsmt_area_sm) + wall_height_m * main_area_sm
 # adjust for different top floor area with 8' ceiling and 1' floor
 volume += tad_in_sm *  9/FT_PER_M
 t.find("House/NaturalAirInfiltration/Specifications/House").attrib["volume"] = str(volume)
+hd.write("\nhouse volume cf: " + str(round(volume * SF_PER_SM * FT_PER_M)))
+
 # calculate highest ceiling height
 # template has 4' pony, so add 1' above grade + 1' header to wall height
-highest_ceiling = (4+1+1)/FT_PER_M + wall_height_m
-t.find("House/NaturalAirInfiltration/Specifications/BuildingSite").attrib["highestCeiling"] = str(highest_ceiling)
+# highest ceiling is best calculated manually
+#highest_ceiling = (4+1+1)/FT_PER_M + wall_height_m
+#t.find("House/NaturalAirInfiltration/Specifications/BuildingSite").attrib["highestCeiling"] =
+# str(highest_ceiling)
 
 hc = t.find("House/Components")
 ef = hc.find("Floor")
 if ta_delta > 0:
     # configure exposed floor
+    efl_m = math.sqrt(tad_in_sm)
     ef.find("Measurements").attrib["area"] = str(tad_in_sm)
-    ef.find("Measurements").attrib["length"] = str(math.sqrt(tad_in_sm))
+    ef.find("Measurements").attrib["length"] = str(efl_m)
+    hd.write("\nexposed floor area, length: "
+                + str(round(tad_in_sm * SF_PER_SM)) + ", "
+                + str(round(efl_m * FT_PER_M)))
 else:
     hc.remove(ef)
 
 m = hc.find("Ceiling/Measurements")
 # eave length
-m.attrib["length"] = str((operim/FT_PER_M)/2)
-ceiling_area_sm = main_area_sm if ta_delta < 0 else main_area_sm + tad_in_sm * ta_sign
+c_len_m = mperim_in_m/2
+m.attrib["length"] = str(c_len_m)
+ceiling_area_sm = main_area_sm if ta_delta < 0 else main_area_sm + tad_in_sm
 m.attrib["area"] = str(ceiling_area_sm)
+hd.write("\nceiling area, length: " +
+            str(round(ceiling_area_sm * SF_PER_SM)) +
+            ", " + str(round(c_len_m * FT_PER_M )))
 
 m = hc.find("Wall/Measurements")
 m.attrib["height"] = str(wall_height_m)
-m.attrib["perimeter"] = str((operim-4)/FT_PER_M)
+m.attrib["perimeter"] = str(mperim_in_m)
+hd.write("\nwall height, perim: " +
+            str(round(wall_height_m * FT_PER_M )) +
+            ", " + str(mperim_in_m * FT_PER_M))
 
 # calculate foundation perim & area
-bsmt_perim = (operim-8)/FT_PER_M
-hc.find("Basement").attrib["exposedSurfacePerimeter"] = str(bsmt_perim)
+hc.find("Basement").attrib["exposedSurfacePerimeter"] = str(bperim_in_m)
 m = hc.find("Basement/Floor/Measurements")
 m.attrib["area"] = str(bsmt_area_sm)
 # H2K errror if perim <= 4*sqrt(area), common ratio is 1.05x
-m.attrib["perimeter"] = str(math.sqrt(bsmt_area_sm)*4 * 1.05)
+# relevant for semis and multiple foundations
+bfp_m = math.sqrt(bsmt_area_sm)*4 * 1.05
+m.attrib["perimeter"] = str(bfp_m)
+hd.write("\nfoundation floor area, perim, exp. surface perim: " +
+            str(round(bsmt_area_sm * SF_PER_SM )) +
+            ", " + str(round(bfp_m * FT_PER_M)) +
+            ", " + str(round(bperim_in_m * FT_PER_M)))
 
 # debug
 #t.write("out.h2k", "UTF-8", True)
@@ -123,9 +160,3 @@ m.attrib["perimeter"] = str(math.sqrt(bsmt_area_sm)*4 * 1.05)
 outfile = "../../" + fileid + ".h2k"
 t.write(outfile, "UTF-8", True)
 #os.system("unix2dos " + outfile)
-
-# copy stick-framed 2x6 house specs
-house_data = "../../" + fileid  + "/" + fileid + "-house-data.txt"
-os.system("cp 2x6-house.txt " + house_data)
-os.system("echo " + info + ">>" + house_data)
-
