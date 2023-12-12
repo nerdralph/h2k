05c1b8fcee1c0b7969ea06c20a47d3b539c91113
nerdralph <ralphdoncaster@gmail.com>
2023-12-11 23:20:43 -0400

    check PVSC year built and style

--- qa/qa.py
+++ qa/qa.py
@@ -2,14 +2,27 @@
 # (c) Ralph Doncaster nerdralph.blogspot.ca
 
 import xml.etree.ElementTree as ET
-import cgi, ctitb, datetime, json, math, os, requests, sys
-
-cgitb.enable()
+import cgi, datetime, json, math, os, requests, sys
 
 print ("Content-type: text/plain\n")
 print ("Solar Si H2k QA scanner alpha")
 
-tree = ET.parse(sys.stdin)
-print ("file: " + h2kfile)
+form = cgi.FieldStorage()
+h2k = form["file"]
+print ("file: " + h2k.filename)
 
+tree = ET.parse(h2k.file)
+pif = tree.find("ProgramInformation/File)
 
+print ("File ID: " + tree.findtext("Identification"))
+tid = pif.find("TaxNumber").text or "no AAN"
+print("AAN: " + tid)
+
+jd = requests.get("https://www.thedatazone.ca/resource/a859-xvcs.json?aan=" + tid).json()[0]
+print(json.dumps(jd))
+
+hse = tree.find("House")
+print("h2k file vs PVSC data")
+specs = hse.find("Specifications")
+print(specs.find("YearBuilt").attrib["value"] + " vs " + jd.get("year_built") )
+print(specs.find("HouseType/English").text + " vs " + jd.get("style") )
